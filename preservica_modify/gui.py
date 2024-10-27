6635e913a9a94cf3372ad8bf7ccce0846fb04f84
CPJPRINCE <c.pj.prince@gmail.com>
2024-10-27 21:05:14 +0000

    gui update 2

--- preservica_modify/gui.py
+++ preservica_modify/gui.py
@@ -14,6 +14,7 @@ class PrintLogger(object):
 class MainGUI(Tk):
     def __init__(self):
         Tk.__init__(self)
+        self.geometry( "400x600")
         self.title("Preservica Mass Modification Tool")
         self.columnconfigure([0,1], weight = 1, minsize=45)
         self.rowconfigure([0,1,2,4],weight = 1, minsize=45)        
@@ -43,16 +44,34 @@ class MainGUI(Tk):
     def login_selection(self):
         loginf = tk.Frame(master=self,relief=RAISED,borderwidth=1)
         loginf.grid(row=2,column=0)
-        userl = tk.Label(master=loginf, text="Username")
-        usere = tk.Entry(master=loginf)
-        passl = tk.Label(master=loginf, text="Password")
-        passe = tk.Entry(master=loginf)
-        serverl = tk.Label(master=loginf, text="Server")
-        servere = tk.Entry(master=loginf)
-        userl.pack()
-        usere.pack()
-        passl.pack()
-        passe.pack()
+        userf = tk.Frame(master=loginf, borderwidth=1)
+        serverf = tk.Frame(master=loginf, borderwidth=1)
+        userf.pack()
+        userl = tk.Label(master=userf, text="Username")
+        usere = tk.Entry(master=userf)
+        passl = tk.Label(master=userf, text="Password")
+        passe = tk.Entry(master=userf)
+        userl.grid(row=0,column=0)
+        usere.grid(row=1,column=0)
+        passl.grid(row=0,column=1)
+        passe.grid(row=1,column=1)
+
+        serverf.pack(side=BOTTOM)
+        serverl = tk.Label(master=serverf, text="Server")
+        servere = tk.Entry(master=serverf)
+        serverl.grid(row=0,column=0)
+        servere.grid(row=1,column=0)
+
+    def xml_selection(self):
+        xmlf = tk.Frame(master=self, relief=RAISED,borderwidth=1)
+        xmlf.grid(row=0,column=1)
+        xmll = tk.Label(master=xmlf, borderwidth=1, text= "XML Method")
+        xmlvar = StringVar()
+        xmlvar.set('flat')
+        options = ['flat', 'exact']
+        xmle = tk.OptionMenu(xmlf,xmlvar,*options)
+        xmll.pack()
+        xmle.pack()
 
     def browse_button_file(self, var):
         filed = filedialog.askopenfile(mode='r',filetypes=[('Excel Files','*.xlsx'),('CSV Files','*.csv')])
@@ -66,6 +85,7 @@ class MainGUI(Tk):
         self.input_section()
         self.mdir_slection()
         self.login_selection()
+        self.xml_selection()
         self.mainloop()
 
 MainGUI().main()
